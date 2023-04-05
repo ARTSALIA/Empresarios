c497427ac9779b220ac87a9aeb03290b4b9799f7
arturoxo456 <arturoxo555salinas@gmail.com>
2023-04-05 02:11:08 -0500

    NUEVO PROYECTO

---
+++ bungalows.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Pie de pagina</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+     <!--Iconos-->
+    <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<section>
+        <div class="contenedor">
+            <h1>Celebra tu gran momento 
+                 en un lugar especial</h1>
+            <h2>Enterate mas aqui!</h2>      
+    </div>
+    
+        <div class="informacion">
+        </div>
+    </div>
+        <div class="informacions">
+        
+        </div>
+
+    </section>
+</body>
+<!--::::Pie de Pagina::::::-->
+<footer class="pie-pagina">
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="img/logo.jpeg" alt="">
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>COMUNIQUESE CON NOSOTROS</h2>
+                <a href="">
+                    ·Teléfono. +51 987 154 124
+                </a> 
+                <a href="">
+                    ·Correo. info@CasaRamirez.com
+                </a>
+                <p>·Dirección. Asociación san juan de Miraflores, Mz B Lt. 2, centro de luciana, Lima - Perú</p>
+            </div>
+            <div class="box">
+            <h2>SOBRE NOSOTROS</h2>
+               <p>·NOSOTROS</p>
+                <p>·BUNGALOWS</p>
+                <p>·ACTIVIDADES</p>
+                <p>·CONTACTANOS</p>
+               </div> 
+            <div class="box">
+                <h2>SIGUENOS</h2>
+                <div class="red-social">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-instagram"></a>
+                </div>
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy; Copyright Casa Bungalow Ramirez <b>-</b> Diseñado por SENATI <b>-</b> Perú</small>
+        </div>
+    </footer>
+</html>
